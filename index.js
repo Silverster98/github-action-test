2eb71cc268a7f330f99ecf7c752b7139c0b0735a
Silvester98 <1539168414@qq.com>
2021-02-10 17:57:22 +0800

    feat: use bulma, and add project page

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
-let app;
+// let app;
 
-window.onload = function () {
-  app = new Vue({
-    el: '#app',
-    data: {
-      firstname: 'Foo',
-      lastname: 'Bar',
-    },
-    computed: {
-      fullname: {
-        get: function() {
-          return this.firstname + ' ' + this.lastname
-        },
+// window.onload = function () {
+//   app = new Vue({
+//     el: '#app',
+//     data: {
+//       firstname: 'Foo',
+//       lastname: 'Bar',
+//     },
+//     computed: {
+//       fullname: {
+//         get: function() {
+//           return this.firstname + ' ' + this.lastname
+//         },
 
-        set: function(newValue) {
-          var names = newValue.split(' ')
-          this.firstname = names[0]
-          this.lastname = names[names.length - 1]
-        }
-      }
-    },
-  })
-}
+//         set: function(newValue) {
+//           var names = newValue.split(' ')
+//           this.firstname = names[0]
+//           this.lastname = names[names.length - 1]
+//         }
+//       }
+//     },
+//   })
+// }
+
+import 'bulma-start/css/main.css'
+import './css/main.css'
+
+// const path = require('path')
+
+// const axios = require('axios')
+// const marked = require('marked')
+// const stylecss = require('./js/stylecss')
+
+// let ownername = 'Zan Wang'
+// let publications = [
+// {
+//   "figure": 'https://picturepan2.github.io/spectre/img/osx-el-capitan.jpg',
+//   "page": '/publications/some',
+//   "title": 'Empower every person and every organization on the planet to achieve more',
+//   "publisher": 'ECCV',
+//   "authors": ['One One', 'Zan Wang', 'Three Three'],
+//   "paper": '/publications/paper.pdf',
+//   "video": 'https://www.youtube.com/',
+//   "code": 'https://github.com/',
+//   "abstract": '',
+//   "bibtex": `@inproceedings{yuan2018rearrangement,
+// title={Rearrangement with nonprehensile manipulation using deep reinforcement learning},
+// author={Yuan, Weihao and Stork, Johannes A and Kragic, Danica and Wang, Michael Y and Hang, Kaiyu},
+// booktitle={2018 IEEE International Conference on Robotics and Automation (ICRA)},
+// pages={270--277},
+// year={2018},
+// organization={IEEE}
+// }`
+// },
+// {
+//   "figure": 'https://picturepan2.github.io/spectre/img/osx-el-capitan.jpg',
+//   "page": '/publications/some',
+//   "title": 'Empower every person and every organization on the planet to achieve more planet to achieve more',
+//   "publisher": 'ECCV',
+//   "authors": ['Zan Wang', 'Three Three', 'One One'],
+//   "paper": '/publications/paper.pdf',
+//   "video": 'https://www.youtube.com/',
+//   "code": 'https://github.com/',
+//   "abstract": '',
+//   "bibtex": `@inproceedings{yuan2018rearrangement,
+// title={Rearrangement with nonprehensile manipulation using deep reinforcement learning},
+// author={Yuan, Weihao and Stork, Johannes A and Kragic, Danica and Wang, Michael Y and Hang, Kaiyu},
+// booktitle={2018 IEEE International Conference on Robotics and Automation (ICRA)},
+// pages={270--277},
+// year={2018},
+// organization={IEEE}
+// }`
+// }
+// ]
